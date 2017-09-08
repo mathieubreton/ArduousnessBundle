b45575a35540b4321f24a0d068f8a0b4a924e327
mathieubreton <mathieu_breton@hotmail.fr>
2017-09-08 15:15:06 +0200

    Add files via upload

--- Form/ard_valueType.php
+++ Form/ard_valueType.php
@@ -6,20 +6,22 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
-class ardTaskType extends AbstractType
+class ard_valueType extends AbstractType
 {
   public function buildForm(FormBuilderInterface $builder, array $options)
   {
     $builder
-      ->add('libelleTask', 'text')
-      ->add('', '')
+      ->add('valueString', 'text')
+      ->add('valueInt', 'text')
+      ->add('valueFloat', 'text')
+      ->add('valueBoolean', 'text')
     ;
   }
 
   public function setDefaultOptions(OptionsResolverInterface $resolver)
   {
     $resolver->setDefaults(array(
-      'data_class' => 'SEPConseil\ArduousnessBundle\Entity\ardTask'
+      'data_class' => 'SEPConseil\ArduousnessBundle\Entity\ard_value'
     ));
   }
 
